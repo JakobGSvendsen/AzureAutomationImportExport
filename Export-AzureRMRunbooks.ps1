732c14325454354bccdd49e04bce30730b818d5c
Jakob Gottlieb Svendsen <JGS@coretech.dk>
2017-08-23 15:52:13 +0200

    examples

--- Export-AzureRMRunbooks.ps1
+++ Export-AzureRMRunbooks.ps1
@@ -10,12 +10,17 @@ Import-Module .\AzureAutomationImportExport
 
 #Export script runbook and child script runbooks (does not work in workflows!)
 
-$RunbookName = "Enable-O365User"
-$OutputFolder = "C:\Temp\ELEU\$RunbookName"
-if(!(Test-PAth $OutputFolder)) {mkdir $OutputFolder | Out-Null}
-. Export-ScriptRunbook -ResourceGroupName $ResourceGroupName -AutomationAccountName $AutomationAccountName -RunbookName $RunbookName -OutputFolder $OutputFolder
 
 break
+
+$Runbooks = Get-AzureRmAutomationRunbook -ResourceGroupName $ResourceGroupName -AutomationAccountName $AutomationAccountName
+foreach($Runbook in $Runbooks){
+    $RunbookName = $Runbook.Name
+    $OutputFolder = "C:\Temp\ELEU\$RunbookName"
+    if(!(Test-PAth $OutputFolder)) {mkdir $OutputFolder | Out-Null}
+    . Export-ScriptRunbook -ResourceGroupName $ResourceGroupName -AutomationAccountName $AutomationAccountName -RunbookName $RunbookName -OutputFolder $OutputFolder
+}
+
 Remove-Module AzureAutomationImportExport
 
 Get-ChildItem -Path $FolderPath | gm
